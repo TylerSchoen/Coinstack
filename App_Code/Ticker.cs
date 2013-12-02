eca849ea13d1ad2185011420a97b7818884e5904
Tyler Schoen <tyler@ethode.com>
2013-12-01 19:47:35 -0500

    Added Ticker Class for JSON from BTC-e

---
+++ App_Code/Ticker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Ticker
+/// </summary>
+public class Ticker
+{
+    public double high { get; set; }
+    public double low { get; set; }
+    public double avg { get; set; }
+    public double vol { get; set; }
+    public double vol_cur { get; set; }
+    public double last { get; set; }
+    public double buy { get; set; }
+    public double sell { get; set; }
+    public int updated { get; set; }
+    public int server_time { get; set; }
+}
+
+public class RootObject
+{
+    public Ticker ticker { get; set; }
+}
